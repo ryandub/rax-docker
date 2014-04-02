a7fd7a38f9df81aef4cd42cd39ae738f80ce4910
Ryan Walker <ryan.walker@rackspace.com>
2014-04-02 12:44:30 -0500

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+# Encoding: utf-8
+name             'rax-docker'
+maintainer       'Rackspace Hosting'
+maintainer_email 'ryan.walker@rackspace.com'
+license          'Apache 2.0'
+description      'Installs/Configures rax-docker'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'docker'
